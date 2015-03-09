f91929564814719a89fb5a34265acd7c3291afe1
Bill Smith <sergolwow@gmail.com>
2015-03-09 02:41:02 +0200

    Initial Commit

---
+++ AsmTest1/Source.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+extern "C" 
+{
+	void sayHello();
+}
+
+//extern void sayHello();
+void main()
+{
+	printf("Hello, what is your name?\n");
+	sayHello();
+	while (getchar() != '\n');
+}
+extern "C"
+void* readName()
+{
+	char* name=(char*)calloc(1,255);
+	scanf("%s", name);
+	while (getchar() != '\n');
+	return name;
+}
